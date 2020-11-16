582517fffc7c85f1403366b6ab6c0b3d68fc0c34
Israel Mesa <mesa.israel50@gmail.com>
2020-11-16 07:27:05 -0500

    loops and arrays

---
+++ typescript-training/03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportsTwo: String[] = ["Climbing", "Swimming", "Futbol"];
+
+sportsTwo.push("Basketball");
+sportsTwo.push("Cycling");
+
+for (let tempSport of sportsTwo) {
+    console.log(tempSport)
+}
