211d3dbdc347f8cd6899cc8113583abb8d0f8c76
Israel Mesa <mesa.israel50@gmail.com>
2020-11-15 22:15:23 -0500

    types

---
+++ typescript-training/02-variables/sample-types.ts
@@ -0,0 +1,10 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "John";
+let lastName: string = "Smith";
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+
+console.log(`Hi ${firstName} ${lastName}`);
